4b2ee323b93015794d3cc2e7b9d7b0e4e52ce011
Shubham Gour <gourshubham6@gmail.com>
2026-08-07 13:44:54 +0530

    Return JSON response

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 OptionPilot Server Running"
+    return jsonify({
+        "app": "OptionPilot",
+        "status": "running",
+        "version": "0.1"
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
